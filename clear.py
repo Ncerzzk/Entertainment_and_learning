b30a732ab08200051fe84816b148c761cec6cb5b
ncerzzk <huangzzk@bupt.edu.cn>
2015-09-02 20:00:45 +0800

    update

---
+++ clear.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+import time
+from db import *
+
+from setting import *
+
+def get_hour():
+    # TIMEADD aim to get Beijing Time
+    return int(time.strftime('%H'))+TIMEADD
+
+def abs(t):
+    if t<0:
+        return t*-1
+    else:
+        return t
+
+db=DB(DBHOST,DBUSER,DBPWD,DBNAME)
+now_time=get_hour()
+now_time=6
+result=db.select('userinfo',{'clear_time':now_time},'uid,today_score,score')
+if result!=1:
+    for i in result:
+        uid=i['uid']
+        today_score=int(i['today_score'])
+        sum_score=int(db.sum('task','score',{'uid':uid})) #task socre_sum
+        score=int(i['score']) #user all score
+        k=(today_score-sum_score)/abs(sum_score)
+        k+=1
+        k*=50
+        db.update('userinfo',{'score':k+score,'today_score':0},{'uid':uid})
+else:
+    print('no need to clear')
+
---
+++ setting.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+
+DBHOST='localhost'
+DBUSER='root'
+DBPWD='6789mm'
+DBNAME='learning'
+
+TIMEADD=0
